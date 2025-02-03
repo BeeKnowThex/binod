e97f361529f3e9bad2b70bbdeb2be7409737db4e
BeeKnowThex <bbiswakarma996@gmail.com>
2025-02-03 20:44:29 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Function to show the selected section and hide others
+function showContent(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+      if (section.id === sectionId) {
+        section.classList.add('active');
+      } else {
+        section.classList.remove('active');
+      }
+    });
+  }
+  
